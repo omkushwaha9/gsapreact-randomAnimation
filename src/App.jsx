3f21d7477659055dc4148cc4318df0faa6a70dc6
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-22 08:56:17 +0530

    Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-import {random} from 'gsap'
 import { useRef } from 'react';
 import { useState } from 'react';
 
 
 const App = () => {
-  useGSAP(()=>{
-    gsap.to(".box",{
-      x:1150,
-      duration:2,
-      delay:0.5,
-      rotate:3600,
+  const randomX = gsap.utils.random(-100, 1500, 100, true);
+  const rotateX = gsap.utils.random(-3600,3600,30, true)
+
+  const [xValue, setXValue] = useState(0);
+  const [roti, setRoti] = useState(0);
+
+  const boxref = useRef()
+
+  useGSAP(() => {
+    gsap.to(boxref.current, {
+      x:xValue,
+      duration: 1,
+      rotate:roti
     })
-  })
+  }, [xValue,rotateX])
 
   return (
     <main>
-      <div className="box"></div>
+      <button onClick={() => {
+       setXValue(randomX)
+       setRoti(rotateX)
+      }}>Animate</button>
+      <div ref={boxref} className="box"></div>
     </main>
   )
 }
 
-export default App
+export default App;
